767e9581c4aa3651cae8b9e77a0741dea6e88f0d
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-02 17:25:02 -0300

    Hice el create del CRUD

--- client/components/FormProject.jsx
+++ client/components/FormProject.jsx
@@ -1,16 +1,62 @@
 import { Formik, Form, Field } from "formik";
+import { useNavigate } from "react-router-dom";
 
-const FormProject = () => {
+const FormProject = ({ project }) => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (values) => {
+    const data = await fetch("http://localhost:4000/projects", {
+      method: "POST",
+      body: JSON.stringify(values),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const respuesta = await data.json();
+    console.log(respuesta);
+    navigate("/");
+  };
+
   return (
     <div className="bg-color2 text-white w-6/12 m-auto justify-center mt-10 rounded-md shadow-md">
-      <p className="text-2xl p-5">New Project</p>
-      <Formik>
+      <p className="text-2xl p-5 text-center">New Project</p>
+      <Formik
+        initialValues={{
+          title: project?.title ?? "",
+          description: project?.description ?? "",
+        }}
+        enableReinitialize={true}
+        onSubmit={async (values, { resetForm }) => {
+          await handleSubmit(values);
+          resetForm();
+        }}
+      >
         <Form>
-          <Field></Field>
+          <label htmlFor="title">Project Title</label>
+          <Field
+            className="bg-gray-700"
+            id="title"
+            name="title"
+            type="text"
+            placeholder="Project Title"
+          />
+          <label htmlFor="description">Project Description</label>
+          <Field
+            className="bg-gray-700"
+            id="description"
+            name="description"
+            placeholder="Project Description"
+            type="text"
+          />
+          <input type="submit" value="Save Project" className="bg-gray-700" />
         </Form>
       </Formik>
     </div>
   );
+};
+
+FormProject.defaultProps = {
+  project: {},
 };
 
 export default FormProject;
--- client/components/Project.jsx
+++ client/components/Project.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Project = ({ project }) => {
   const { title, description } = project;
   return (
-    <div className="bg-color1 w-96 p-3 text-white rounded-md shadow-md">
+    <div className="bg-color1 w-96 p-3 mb-6 text-white rounded-md shadow-md">
       <p className="text-center text-2xl mb-3">{title}</p>
       <p>{description}</p>
     </div>
